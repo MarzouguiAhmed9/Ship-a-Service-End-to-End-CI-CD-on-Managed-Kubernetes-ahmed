79cc651909cb599781e8fb63f7021ccbc69c1915
MarzouguiAhmed9 <ahmed.marzougui@esprit.tn>
2025-10-19 14:11:53 +0100

    go func task3

---
+++ app/src/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+    "encoding/json"
+    "net/http"
+    "os"
+)
+
+func main() {
+    http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+        sysEnv := os.Getenv("SYS_ENV")
+        payload := map[string]string{
+            "status":  "ok",
+            "SYS_ENV": sysEnv,
+        }
+        w.Header().Set("Content-Type", "application/json")
+        json.NewEncoder(w).Encode(payload)
+    })
+
+    http.ListenAndServe(":8080", nil)
+}
