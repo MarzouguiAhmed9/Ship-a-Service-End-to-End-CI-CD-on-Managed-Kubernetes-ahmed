5148129cda51b90d0003d4d80a77f5ca7502ed61
MarzouguiAhmed9 <ahmed.marzougui@esprit.tn>
2025-10-22 20:43:05 +0100

    metric go

--- app/src/main.go
+++ app/src/main.go
@@ -2,11 +2,16 @@ package main
 
 import (
     "encoding/json"
+    "fmt"
     "net/http"
     "os"
+    "sync/atomic"
 )
 
+var requestCount uint64 // counter
+
 func main() {
+    // Health endpoint
     http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
         sysEnv := os.Getenv("SYS_ENV")
         payload := map[string]string{
@@ -15,6 +20,16 @@ func main() {
         }
         w.Header().Set("Content-Type", "application/json")
         json.NewEncoder(w).Encode(payload)
+    })
+
+    http.HandleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
+        w.Header().Set("Content-Type", "text/plain")
+        fmt.Fprintf(w, "my_app_requests_total %d\n", atomic.LoadUint64(&requestCount))
+    })
+
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        atomic.AddUint64(&requestCount, 1)
+        w.Write([]byte("Hello World!\n"))
     })
 
     http.ListenAndServe("0.0.0.0:8080", nil)
